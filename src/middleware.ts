cf6de0d357c90099d4471adf88cee2060c3b1907
Uma krishna kanth <umakrishnakanthchokkapu15@gmail.com>
2026-05-09 15:27:30 +0530

    fix: middleware safe return

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/simulate(.*)",
@@ -10,6 +11,7 @@ export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
     await auth.protect();
   }
+  return NextResponse.next();
 });
 
 export const config = {
